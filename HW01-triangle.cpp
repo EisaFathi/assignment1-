8fe27e0cd85220756f0827c3afebaf5a429195f8
EISA <eisafathi2002@gmail.com>
2025-03-01 02:36:19 +0330

    HW01-CODE

--- HW01-triangle.cpp
+++ HW01-triangle.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 bool isTriangle(int a, int b, int c) {
 
-    return (a + b > c) && (a + c > b) && (b + c > a);
+    return (a + b > c) && (a + c > b) && (b + c > a) &&
+           (abs(a - b) < c) && (abs(a - c) < b) && (abs(b - c) < a);
 }
 
 int main() {
     int a, b, c;
-    cout << "Enter three numbers: ";
     cin >> a >> b >> c;
 
     if (isTriangle(a, b, c))
