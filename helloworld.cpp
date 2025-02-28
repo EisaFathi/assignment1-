118da46d286478455401fbb6f66ba9c2daa5e24d
EISA <eisafathi2002@gmail.com>
2025-03-01 02:19:48 +0330

    version2

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
 int main() {
+
     std::cout << "Hello, World!" << std::endl;
+    std::cout << " Eisa Fathi " << std::endl;
+    std::cout << "Student ID: 400101718 " << std::endl;
+
     return 0;
 }
